d15220919b8d7544ea7b9abc8e73cc86977107fc
bthanhnhut <bthanhnhut10@gmail.com>
2023-11-25 17:58:17 +0700

    thêm thuộc tính cho vi phạm hư sách

--- src/main/java/com/example/API_ThuVien/model/Viphamhusach.java
+++ src/main/java/com/example/API_ThuVien/model/Viphamhusach.java
@@ -1,6 +1,8 @@
 package com.example.API_ThuVien.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,8 +18,10 @@ import lombok.NoArgsConstructor;
 @Table(name = "viphamhusach")
 public class Viphamhusach {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id_vipham;
 	private Integer id_tailieu;
 	private Integer id_muon;
 	private String mota;
+	private Integer tongtien;
 }
